0f739a71baac70eade3de179917cefb074ee9bb5
Altela <altela_pramardhika@yahoo.com>
2022-04-23 01:18:39 +0800

    Added harga and box in hitung_keramik

--- hitung_keramik.py
+++ hitung_keramik.py
@@ -1,4 +1,4 @@
-def hitung_jumlah_keramik(panjang_lantai_meter, lebar_lantai_meter, panjang_keramik, lebar_keramik, gap_semen):
+def hitung_jumlah_keramik(panjang_lantai_meter, lebar_lantai_meter, pilihan_keramik, gap_semen, box_keramik, harga_box):
     # Konversi space dari meter ke centimeter
     panjang_lantai_cm = panjang_lantai_meter * 100
     lebar_lantai_cm = lebar_lantai_meter * 100
@@ -7,9 +7,20 @@ def hitung_jumlah_keramik(panjang_lantai_meter, lebar_lantai_meter, panjang_kera
     keliling_space = panjang_lantai_cm * lebar_lantai_cm
 
     # Menghitung keliling keramik
-    keliling_keramik = (panjang_keramik + gap_semen) * (lebar_keramik + gap_semen)
+    if pilihan_keramik == 1:
+        keliling_keramik = (20 + gap_semen) * (20 + gap_semen)
+    elif pilihan_keramik == 2:
+        keliling_keramik = (30 + gap_semen) * (30 + gap_semen)
+    elif pilihan_keramik == 3:
+        keliling_keramik = (40 + gap_semen) * (40 + gap_semen)
+    elif pilihan_keramik == 4:
+        keliling_keramik = (45 + gap_semen) * (45 + gap_semen)
+    elif pilihan_keramik == 5:
+        keliling_keramik = (50 + gap_semen) * (50 + gap_semen)
 
     # Menghitung kebutuhan keramik
-    jumlah_keramik = keliling_space / (keliling_keramik + gap_semen)
+    jumlah_keramik = int(keliling_space / keliling_keramik)
+    jumlah_box = int(jumlah_keramik / box_keramik)
+    total_harga = int(harga_box * jumlah_box)
 
-    print("Estimasi jumlah keramik yang diperlukan adalah", int(jumlah_keramik))
+    print(f"Estimasi jumlah keramik yang diperlukan adalah {int(jumlah_keramik)}, sehingga anda perlu membeli sebanyak {int(jumlah_box)} dan membutuhkan dana sekitar Rp{total_harga:,}")
--- menu_sub_keramik.py
+++ menu_sub_keramik.py
@@ -3,19 +3,25 @@ from hitung_keramik import *
 # Mulai program dengan while repeat
 def submenu_keramik():
     from menu_main import main_menu
-    running = True
-    while running:
+    while True:
         print("\n========== Sub Menu Kalkulasi Hitung Material Keramik ==========\n")
         print("Input space yang tersedia")
         panjang_lantai_meter = int(input("Panjang space lantai dalam satuan meter : "))
         lebar_lantai_meter = int(input("Lebar space lantai dalam satuan meter : "))
 
-        print("\nMasukkan informasi keramik")
-        panjang_keramik = int(input("Panjang keramik dalam satuan centimeter : "))
-        lebar_keramik = int(input("Lebar keramik dalam satuan centimeter : "))
+        print("\nBerapa ukuran keramik yang digunakan?")
+        print("1. Ukuran kecil (20 x 20)")
+        print("2. Ukuran standar (30 x 30)")
+        print("3. Ukuran sedang (40 x 40)")
+        print("4. Ukuran besar (45 x 45)")
+        print("5. Ukuran ekstra besar (50 x 50)") 
+        pilihan_keramik = int(input("Masukkan pilihan anda : "))
 
         gap_semen = int(input("Jarak semen penghubung dalam satuan centimeter : "))
-        hitung_jumlah_keramik(panjang_lantai_meter, lebar_lantai_meter, panjang_keramik, lebar_keramik, gap_semen)
+        box_keramik = int(input("Jumlah keramik dalam satu box pembelian : "))
+        harga_box = int(input("Harga per box : "))
+
+        hitung_jumlah_keramik(panjang_lantai_meter, lebar_lantai_meter, pilihan_keramik, gap_semen, box_keramik, harga_box)
 
         cek_status = str(input("Apakah ingin menghitung estimasi keramik lagi? (y/n) : "))
         if cek_status.lower() != "y":
