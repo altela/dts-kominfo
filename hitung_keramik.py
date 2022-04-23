a8ef45e9d5c3363d546776321dbe8a8071ff1de1
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2022-04-23 13:26:19 +0800

    Fixed keramik box rounding

--- hitung_keramik.py
+++ hitung_keramik.py
@@ -8,19 +8,32 @@ def hitung_jumlah_keramik(panjang_lantai_meter, lebar_lantai_meter, pilihan_kera
 
     # Menghitung keliling keramik
     if pilihan_keramik == 1:
+        # Keramik ukuran 20 x 20
         keliling_keramik = (20 + gap_semen) * (20 + gap_semen)
     elif pilihan_keramik == 2:
+        # Keramik ukuran 30 x 30
         keliling_keramik = (30 + gap_semen) * (30 + gap_semen)
     elif pilihan_keramik == 3:
+        # Keramik ukuran 40 x 40
         keliling_keramik = (40 + gap_semen) * (40 + gap_semen)
     elif pilihan_keramik == 4:
+        # Keramik ukuran 45 x 45
         keliling_keramik = (45 + gap_semen) * (45 + gap_semen)
     elif pilihan_keramik == 5:
+        # Keramik ukuran 50 x 50
         keliling_keramik = (50 + gap_semen) * (50 + gap_semen)
 
     # Menghitung kebutuhan keramik
     jumlah_keramik = int(keliling_space / keliling_keramik)
-    jumlah_box = int(jumlah_keramik / box_keramik)
+
+    # Menghitung berapa jumlah box yang harus dibeli berdasarkan banyaknya keramik
+    # Jika hasil bagi keramik dengan keramik per box memiliki sisa bagi, maka pembeli harus menambahkan 1 box untuk mengcover kebutuhan sisa
+    # Jika hasik bagi tidak memiliki sisa, berarti tidak memerlukan tambahan 1 box untuk sisa kebutuhan keramik
+    if jumlah_keramik % box_keramik == False:
+        jumlah_box = int(jumlah_keramik / box_keramik)
+    else:
+        jumlah_box = int((jumlah_keramik / box_keramik) + 1)
+
     total_harga = int(harga_box * jumlah_box)
 
-    print(f"Estimasi jumlah keramik yang diperlukan adalah {int(jumlah_keramik)}, sehingga anda perlu membeli sebanyak {int(jumlah_box)} dan membutuhkan dana sekitar Rp{total_harga:,}")
+    print(f"\nEstimasi jumlah keramik yang diperlukan adalah {int(jumlah_keramik):,} pcs, sehingga anda perlu membeli sebanyak {int(jumlah_box)} box dan membutuhkan dana sekitar Rp{total_harga:,}")
